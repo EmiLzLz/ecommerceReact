7ec047b1e25e97e54ae6391aabd8ceea337a205a
emiliano <contact@emilianoll.com>
2023-08-23 18:17:02 -0600

    Add cart logic to remove or add products, delete all products, show the total and quantity of each product

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,19 +2,84 @@ import React, { useEffect, useState } from "react";
 import { useApi } from "../context/ApiContext";
 
 function Cart() {
-  const {cart, removeFromCart} = useApi();
-  
+  const { cart, removeFromCart, removeAllFromCart, updateProductQuantity } =
+    useApi();
+  const [total, setTotal] = useState(0);
+
+  const handleAdd = (productId) => {
+    updateProductQuantity(productId, 1);
+  };
+
+  const handleRemove = (productId) => {
+    updateProductQuantity(productId, -1);
+  };
+
+  const handleRemoveFromCart = (productId) => {
+    removeFromCart(productId);
+    if(cart.length === 1){
+      setTotal(0);
+    }
+  }
+
+  const handleRemoveAllFromCart = () => {
+    removeAllFromCart();
+    setTotal(0);
+  }
+
+  useEffect(() => {
+    let totalPay = 0;
+
+    if (cart !== null) {
+      cart.forEach((product) => {
+        totalPay += product.price * product.quantity;
+
+        setTotal(totalPay);
+      });
+    }
+    else{
+      return 0;
+    }
+  }, [cart]);
 
   return (
-    <div>
-      <ul>
-        {cart.map((cartProduct) => (
-          <li key={cartProduct.id}>{cartProduct.title}
-          <button onClick={() => removeFromCart(cartProduct.id)}>DELETE FROM CART</button>
-          </li>
-          
-        ))}
-      </ul>
+    <div className="cart">
+      <div className="cart-container">
+        <div className="cart-body">
+          <div className="cart-products">
+            {cart.map((cartProduct) => (
+              <div key={cartProduct.id} className="cart-product">
+                <div className="product-cart-img">
+                  <img src="" alt="" />
+                </div>
+                <div className="product-cart-info">
+                  <p>{cartProduct.title}</p>
+                  <p>${cartProduct.price}</p>
+                  <div className="addMore">
+                    <button onClick={() => handleRemove(cartProduct.id)}>
+                      -
+                    </button>
+                    <p>{cartProduct.quantity}</p>
+                    <button onClick={() => handleAdd(cartProduct.id)}>+</button>
+                  </div>
+                </div>
+                <button onClick={() => handleRemoveFromCart(cartProduct.id)}>
+                  DELETE FROM CART
+                </button>
+              </div>
+            ))}
+          </div>
+
+          <div className="cart-actions">
+            <div className="cart-total">
+              <p>TOTAL: ${total.toFixed(2)} </p>
+              <p></p>
+              <div className="deleteAll">
+                <button onClick={() => handleRemoveAllFromCart()}>DELETE ALL</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/context/ApiContext.jsx
+++ src/context/ApiContext.jsx
@@ -5,6 +5,7 @@ const ApiContext = createContext();
 function ApiProvider({ children }) {
   const [products, setProducts] = useState([]);
   const [cart, setCart] = useState([]);
+  const initialState = [];
 
   useEffect(() => {
     try {
@@ -32,16 +33,21 @@ function ApiProvider({ children }) {
   };
 
   const addToCart = (product) => {
-    if(cart.some((cartProduct) => cartProduct.id === product.id)){
-      return;
+    // Check if the product is already in the cart
+    const productIndex = cart.findIndex((p) => p.id === product.id);
+
+    if (productIndex === -1) {
+      // Product is not in the cart, add it with quantity 1
+      const updatedCart = [...cart, { ...product, quantity: 1 }];
+      setCart(updatedCart);
+      localStorage.setItem("cart", JSON.stringify(updatedCart));
+    } else {
+      // Product is already in the cart, increment its quantity
+      const updatedCart = [...cart];
+      updatedCart[productIndex].quantity += 1;
+      setCart(updatedCart);
+      localStorage.setItem("cart", JSON.stringify(updatedCart));
     }
-
-    // add product to the cart state
-    const updatedCart = [...cart, product];
-    setCart(updatedCart);
-
-    //save the updated cart to localStorage
-    localStorage.setItem("cart", JSON.stringify(updatedCart));
   };
 
   const removeFromCart = (productId) => {
@@ -52,7 +58,29 @@ function ApiProvider({ children }) {
     //save the updated cart
     localStorage.setItem("cart", JSON.stringify(updatedCart));
 
-    return{...cart, updatedCart}
+    return { ...cart, updatedCart };
+  };
+
+  const removeAllFromCart = () => {
+    setCart(initialState);
+    localStorage.setItem("cart", JSON.stringify(initialState));
+    return cart;
+  };
+
+  const updateProductQuantity = (productId, quantityChange) => {
+    const updatedCart = cart.map((cartProduct) => {
+      if (cartProduct.id === productId) {
+        const newQuantity = cartProduct.quantity + quantityChange;
+        if (newQuantity >= 1 && newQuantity <= cartProduct.rating.count) {
+          return { ...cartProduct, quantity: newQuantity };
+        }
+        console.log(cartProduct.quantity);
+      }
+      return cartProduct;
+    });
+
+    setCart(updatedCart);
+    localStorage.setItem("cart", JSON.stringify(updatedCart));
   };
 
   // expose function
@@ -61,6 +89,8 @@ function ApiProvider({ children }) {
     cart,
     addToCart,
     removeFromCart,
+    removeAllFromCart,
+    updateProductQuantity,
   };
 
   return (
